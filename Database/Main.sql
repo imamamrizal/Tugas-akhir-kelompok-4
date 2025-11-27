384132928c3cd058fed8ba68ce5f0c1b1a0ef97f
imamamrizal <imam.amrizal.2509357@students.um.ac.id>
2025-11-27 09:03:06 +0700

    Update Main.sql

--- Database/Main.sql
+++ Database/Main.sql
@@ -28,6 +28,7 @@ CREATE TABLE IF NOT EXISTS `main` (
   `Nomor Polisi` varchar(20) NOT NULL,
   `wkt_masuk` timestamp NOT NULL,
   `wkt_keluar` timestamp NOT NULL,
+  `status` varchar(10) NOT NULL,
   `harga` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
